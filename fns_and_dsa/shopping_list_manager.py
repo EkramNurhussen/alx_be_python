61e06525356d847d95d36c61397b453e1300c3a2
Ekram Nurhussen <88303005+EkramNurhussen@users.noreply.github.com>
2025-07-27 21:03:01 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -17,10 +17,10 @@ def main():
             continue
 
         if choice == 1:
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == 2:
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
